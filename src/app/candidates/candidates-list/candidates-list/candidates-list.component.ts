b69ebe53177659b51b7bf42c9c3bd0243b8076e7
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-27 00:31:05 -0300

    chore: added debounceTime at the filter

--- src/app/candidates/candidates-list/candidates-list/candidates-list.component.ts
+++ src/app/candidates/candidates-list/candidates-list/candidates-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { CandidatesListService } from '../../candidate/candidate.service';
 import { Candidates } from '../../candidate/candidates';
@@ -13,6 +14,7 @@ import { Candidates } from '../../candidate/candidates';
 export class CandidatesListComponent implements OnInit {
   candidates: MatTableDataSource<Candidates[]>;
   displayedColumns: string[] = ['name', 'email', 'mobile', 'createdBy'];
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private candidatesService: CandidatesListService) {}
 
@@ -20,9 +22,10 @@ export class CandidatesListComponent implements OnInit {
     this.candidatesService.listAllCandidates().subscribe((cn: Candidates) => {
       this.candidates = new MatTableDataSource<Candidates[]>(cn.data);
     });
+    this.debounce.pipe(debounceTime(300)).subscribe(f => this.filter(f));
   }
 
-  applyFilter(filterValue: string) {
+  filter(filterValue: string) {
     this.candidates.filter = filterValue.trim().toLowerCase();
   }
 }
